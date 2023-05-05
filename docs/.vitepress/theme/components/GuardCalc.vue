d16ec73052118e8c5486138aa4ff865520383d7f
s21 <smallzloj@gmail.com>
2023-05-05 07:58:14 +0000

    change path

--- docs/.vitepress/theme/components/GuardCalc.vue
+++ docs/.vitepress/theme/components/GuardCalc.vue
@@ -1,22 +1,50 @@
 <script setup>
 import { ref,computed,onMounted  } from 'vue'
 
-import monsterData from './resources/monsters.json'
+// import monsterData from './resources/monsters.json'
 
-const data = ref(null)
-// const mobData = JSON.parse(monsterData)
+// const data = ref(null)
+// // const mobData = JSON.parse(monsterData)
+
+// onMounted(() => {
+// console.log(monsterData)
+// console.log('mounted')
+//})
+
+const data = ref(null);
+
+async function loadMonsters() {
+
+let options = {
+  // mode: 'no-cors',
+  method:'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }
+}
+// Fake api for making post requests
+let fetchRes = fetch("/monsters.json",options)
+fetchRes.then(res =>
+    res.json()).then(d => {
+        data.value = d;
+    })
+}
+
 
 onMounted(() => {
-console.log(monsterData)
-console.log('mounted')
+console.log('mounted'),
+loadMonsters()
+console.log(data)
 })
+
 </script>
 
 <template>
 
   <div>
     <div class="text-gray-700">
-      <pre>{{ monsterData.mobs }}</pre>
+      <pre>{{ data }}</pre>
         <!-- {{data}} -->
         <!-- Component Start -->
         <!-- Component End  -->
